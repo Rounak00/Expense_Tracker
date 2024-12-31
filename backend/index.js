a60a361cd931adcf04cb9a94e7430fbda80ea97c
Rounak00 <mukherjeerounak10@gmail.com>
2024-12-31 15:30:29 +0530

    Git ignore setup

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,19 @@ import {secret} from "./config/secrets.js";
 import {connectDB} from "./utils/connectDB.js";
 import mergeResolver from "./graphql/resolvers/index.js";
 import mergeTypeDef from "./graphql/typeDefs/index.js";
+import passport from "passport";
+import session from "express-session";
+import ConnectMongo from "connect-mongodb-session";
 
 const app=express();
 const httpServer=http.createServer(app);
+
+const MongoDBStore=ConnectMongo(session);
+const store = new MongoDBStore({
+    uri:secret.DB_URL,
+    collection:"sessions",
+})
+store.on("error",(err)=>console.log(err));
 const server=new ApolloServer({
     typeDefs:mergeTypeDef,
     resolvers:mergeResolver,
