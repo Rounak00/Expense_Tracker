f94279164ca60de0725c0894ca2d3f9aa5874a34
Rounak00 <mukherjeerounak10@gmail.com>
2025-01-01 12:08:58 +0530

    Added basic pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { BrowserRouter } from 'react-router-dom'
+import GridBackground from './components/ui/GridBackground.jsx'
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<GridBackground>
+				<App />
+			</GridBackground>
+		</BrowserRouter>
+	</React.StrictMode>
+);
