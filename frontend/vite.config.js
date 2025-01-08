3ced06f1f74762d631dce672f4fb0ca4590fb21d
Rounak-Mukherjee-04 <web.dev.mi06@gmail.com>
2025-01-08 21:05:44 +0530

    Added PWA feature

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,61 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touc-icon.png", "masked-icon.png"],
+  manifest: {
+    name: "Expense Tracker",
+    short_name: "Expense Tracker",
+    description: "An app that can track your expenses.",
+    icons: [
+      {
+        src: "./maskable-icon.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "./maskable-icon.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/maskable-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "./maskable-icon.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "./maskable-icon.png",
+        sizes: "256x256",
+        type: "image/png",
+        purpose: "icon",
+      },
+      {
+        src: "./maskable-icon.png",
+        sizes: "384x384",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#181818",
+    background_color: "#e8eac2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
